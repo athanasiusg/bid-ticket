c069e242ac26b5f5e697d01b410b647bf2a7fff9
athanasiusg <athanasius.georgy@gmail.com>
2026-02-25 00:56:34 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>BidTicket</div>;
+  return (
+    <div style={{ padding: 40, fontFamily: "system-ui" }}>
+      <h1>BidTicket</h1>
+      <p>Your Next.js app is running correctly.</p>
+    </div>
+  );
 }
